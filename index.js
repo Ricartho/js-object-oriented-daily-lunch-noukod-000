0ea6444321df694de7b380c7b3f7db198c7c6d21
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-23 23:11:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ class Neighborhood{
   }
 
   deliveries(){
-    return store.deliveries.filter((delivery) => delivery.neighborhoodId === this.id );
+    return store.deliveries.filter(delivery => delivery.neighborhoodId === this.id );
   }
 
   customers(){
-    return store.customers.filter((customer) => customer.neighborhoodId === this.id );
+    return store.customers.filter(customer => customer.neighborhoodId === this.id );
   }
 
   meals(){
